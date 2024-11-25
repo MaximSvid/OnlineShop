bed094643820846ee0bf6e5f7cf871b1ae344f84
Maxim Svidrak <maximsvid@gmail.com>
2024-11-25 11:06:11 +0100

    Initial Commit

---
+++ ProjectWoche2Gr4/ProjectWoche2Gr4App.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectWoche2Gr4App.swift
+//  ProjectWoche2Gr4
+//
+//  Created by Maxim Svidrak on 25.11.24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectWoche2Gr4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
