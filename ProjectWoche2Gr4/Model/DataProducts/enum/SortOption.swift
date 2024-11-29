79714c333eba3a7c2b3a2e1a30b05eea08de8188
Maxim Svidrak <maximsvid@gmail.com>
2024-11-29 10:45:34 +0100

    sort done

---
+++ ProjectWoche2Gr4/Model/DataProducts/enum/SortOption.swift
@@ -0,0 +1,15 @@
+//
+//  SortOption.swift
+//  ProjectWoche2Gr4
+//
+//  Created by Maxim Svidrak on 29.11.24.
+//
+
+
+ enum SortOption {
+        case title
+        case price
+        case rating
+        case category
+        case reviews
+    }
--- ProjectWoche2Gr4/Views/FavoriteView.swift
+++ ProjectWoche2Gr4/Views/FavoriteView.swift
@@ -10,18 +10,49 @@ import SwiftData
 
 struct FavoriteView: View {
     @Environment(\.modelContext) private var context
-    @Query private var favoriteProducts: [Products]
+//    @Query(sort: \Products.title) private var favoriteProducts: [Products]
     @ObservedObject var productsViewModel: ProductsViewModel
     
+    @State private var sortOption: SortOption = .title
+    
+    // sort
+    
+    @Query(sort: \Products.price) private var productsByName: [Products]
+    @Query(sort: \Products.price) private var productsByPrice: [Products]
+    @Query(sort: \Products.price) private var productsByRating: [Products]
+    @Query(sort: \Products.price) private var productsByCategory: [Products]
+    @Query(sort: \Products.price) private var productsByReviews: [Products]
+    
+    
+   
+    
+    var displayedProducts: [Products] {
+            switch sortOption {
+            case .title:
+                return productsByName
+            case .price:
+                return productsByPrice
+            case .rating:
+                return productsByName.sorted { $0.rating.rate > $1.rating.rate }
+            case .category:
+                return productsByName.sorted { $0.category < $1.category }
+            case .reviews:
+                return productsByName.sorted { $0.rating.count > $1.rating.count }
+            }
+        }
+    
+    
+    
     var body: some View {
+        
         NavigationStack {
             VStack {
-                if favoriteProducts.isEmpty {
+                if displayedProducts.isEmpty {
                     Text("No favorite products yet.")
                         .foregroundStyle(.gray)
                         .font(.headline)
                 } else {
-                    List (favoriteProducts) {product in
+                    List (displayedProducts) {product in
                         HStack {
                             AsyncImage(url: URL(string: product.image)) {
                                 image in
@@ -49,7 +80,7 @@ struct FavoriteView: View {
                                 productsViewModel.removeFromFavorite(product: product, context: context)
                                 
                             }) {
-
+                                
                                 Image(systemName: "trash")
                                     .font(.footnote)
                                     .foregroundStyle(.red)
@@ -63,14 +94,38 @@ struct FavoriteView: View {
                     Text("Favorite")
                         .font(.title.bold())
                 }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Menu {
+                        Button("Sort by Name") {
+                            sortOption = .title
+                        }
+                        Button("Sort by Price") {
+                            sortOption = .price
+                        }
+                        Button("Sort by Category") {
+                            sortOption = .category
+                        }
+                        Button("Sort by Rating") {
+                            sortOption = .rating
+                        }
+                        Button("Sort by Reviews") {
+                            sortOption = .reviews
+                        }
+                    } label: {
+                        Image(systemName: "line.3.horizontal.decrease")
+                            .foregroundStyle(.black)
+                    }
+                }
             }
         }
         
     }
-
+    
 }
 
 
 #Preview {
     //    FavoriteView()
 }
+
