4024fad55cd55c9820f1892cbd27d1ccff11603d
Hamzah <rootmoodrm@gmail.com>
2024-11-29 10:58:28 +0100

    ui-chekoutsheet

--- ProjectWoche2Gr4/Views/CartView/CheckoutSheetView.swift
+++ ProjectWoche2Gr4/Views/CartView/CheckoutSheetView.swift
@@ -1,52 +1,162 @@
-//
-//  CheckoutSheetView.swift
-//  ProjectWoche2Gr4
-//
-//  Created by Hamzah on 27.11.24.
-//
-
 import SwiftUI
 
 struct CheckoutSheetView: View {
     @Environment(\.presentationMode) var presentationMode
+    @State private var name: String = ""
     @State private var phoneNumber: String = ""
     @State private var bankNumber: String = ""
-    @State private var showAlert = false
+    @State private var birthdate: Date = Date()
+    @State private var address: String = ""
     @State private var showEndView = false
 
     var body: some View {
         NavigationView {
-            Form {
-                Section(header: Text("Contact Information")) {
-                    TextField("Phone Number", text: $phoneNumber)
-                        .keyboardType(.phonePad)
-                    TextField("Bank Number", text: $bankNumber)
-                        .keyboardType(.numberPad)
+            ZStack {
+                // Hintergrundfarbe
+                Color.white
+                    .edgesIgnoringSafeArea(.all)
+
+                VStack(spacing: 20) {
+                    // Titel und Beschreibung
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Checkout")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(.black)
+                    }
+                    .padding()
+
+                    // Formular
+                    Form {
+                        Section(header: Text("Contact Information").foregroundColor(.black)) {
+                            HStack {
+                                Image(systemName: "person.fill")
+                                    .foregroundColor(.black)
+                                    .padding(.trailing, 8)
+                                TextField("Name", text: $name)
+                                    .foregroundColor(.black)
+                                    .padding(.vertical, 8)
+                            }
+                            .frame(height: 50)
+                            .background(Color.white)
+                            .cornerRadius(8)
+                            .padding(.horizontal)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color.gray, lineWidth: 1)
+                            )
+
+                            HStack {
+                                Image(systemName: "phone.fill")
+                                    .foregroundColor(.black)
+                                    .padding(.trailing, 8)
+                                TextField("Phone Number", text: $phoneNumber)
+                                    .keyboardType(.phonePad)
+                                    .foregroundColor(.black)
+                                    .padding(.vertical, 8)
+                            }
+                            .frame(height: 50)
+                            .background(Color.white)
+                            .cornerRadius(8)
+                            .padding(.horizontal)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color.gray, lineWidth: 1)
+                            )
+
+                            HStack {
+                                Image(systemName: "creditcard.fill")
+                                    .foregroundColor(.black)
+                                    .padding(.trailing, 8)
+                                TextField("Bank Number", text: $bankNumber)
+                                    .keyboardType(.numberPad)
+                                    .foregroundColor(.black)
+                                    .padding(.vertical, 8)
+                            }
+                            .frame(height: 50)
+                            .background(Color.white)
+                            .cornerRadius(8)
+                            .padding(.horizontal)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color.gray, lineWidth: 1)
+                            )
+
+                            HStack {
+                                Image(systemName: "calendar")
+                                    .foregroundColor(.black)
+                                    .padding(.trailing, 8)
+                                DatePicker("Birthdate", selection: $birthdate, displayedComponents: .date)
+                                    .labelsHidden()
+                                    .datePickerStyle(CompactDatePickerStyle())
+                                    .accentColor(.black)
+                            }
+                            .background(Color.white)
+                            .cornerRadius(8)
+                            .padding(.horizontal)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color.gray, lineWidth: 1)
+                            )
+
+                            HStack {
+                                Image(systemName: "house.fill")
+                                    .foregroundColor(.black)
+                                    .padding(.trailing, 8)
+                                TextField("Address", text: $address)
+                                    .foregroundColor(.black)
+                                    .padding(.vertical, 8)
+                            }
+                            .frame(height: 50)
+                            .background(Color.white)
+                            .cornerRadius(8)
+                            .padding(.horizontal)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color.gray, lineWidth: 1)
+                            )
+                        }
+                    }
+                    .padding(.horizontal)
+                    
+                    
+
+                    // Submit-Button
+                    Button(action: {
+                        let formattedBirthdate = formatDate(birthdate)
+                        let userInfo = "Name: \(name), Phone: \(phoneNumber), Bank: \(bankNumber), Birthdate: \(formattedBirthdate), Address: \(address)"
+                        showEndView = true
+                    }) {
+                        Text("Submit")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.black)
+                            .cornerRadius(10)
+                    }
+                    .padding(.horizontal)
+
+                    Spacer()
                 }
-
-                Button(action: {
-                    let userInfo = "Name: \(phoneNumber), Bank: \(bankNumber)"
-                    showEndView = true
-                }) {
-                    Text("Submit")
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.black)
-                        .cornerRadius(10)
-                }
-
-
+                .padding(.bottom, 20)
             }
-            
             .navigationBarTitle("Checkout", displayMode: .inline)
             .navigationBarItems(trailing: Button("Close") {
                 presentationMode.wrappedValue.dismiss()
             })
             .fullScreenCover(isPresented: $showEndView) {
-                EndView(userInfo: "Name: \(phoneNumber), Bank: \(bankNumber)")
+                EndView(userInfo: "Name: \(name), Phone: \(phoneNumber), Bank: \(bankNumber), Birthdate: \(formatDate(birthdate)), Address: \(address)")
             }
         }
+    }
+
+    // Helper function to format the date
+    private func formatDate(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        return formatter.string(from: date)
     }
 }
 
@@ -55,3 +165,4 @@ struct CheckoutSheetView_Previews: PreviewProvider {
         CheckoutSheetView()
     }
 }
+
