b257da9b45d2ee84a7096cfdfb022844bc3010bd
Maxim Svidrak <maximsvid@gmail.com>
2024-12-02 10:48:23 +0100

    AutchView

--- ProjectWoche2Gr4/ViewModel/AuthViewModel.swift
+++ ProjectWoche2Gr4/ViewModel/AuthViewModel.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import SwiftData
 
 @MainActor
 class AuthViewModel: ObservableObject { // ich beobachte hier ob der User eingeloggt ist
@@ -13,22 +14,60 @@ class AuthViewModel: ObservableObject { // ich beobachte hier ob der User eingel
         return AuthModel(username: usernameInput, password: passwordInput)
     }
 
-    func login() {
-        if authModel.validateCredentials() {
-            user = User(username: usernameInput, password: passwordInput)
-            isLoggedIn = true
-            loginError = ""
-        } else {
-            loginError = "Invalid username or password"
+    func login(context: ModelContext) {
+        let fetchDescription = FetchDescriptor<User>(
+            predicate: #Predicate {
+                $0.username == usernameInput && $0.password == passwordInput
+            }
+        )
+        do {
+            let users = try context.fetch(fetchDescription)
+            if let loggedInUser = users.first {
+                user = loggedInUser
+                isLoggedIn = true
+                loginError = ""
+            } else {
+                loginError = "Invalid username or password"
+            }
+        } catch {
+            loginError = "Error fetching users: \(error)"
         }
     }
-
-    func register() {
-        // Hier Registrierung abfertigen
-        user = User(username: usernameInput, password: passwordInput)
-        isLoggedIn = true
-        loginError = ""
+    
+    func register(context: ModelContext) {
+        let fetchDescription = FetchDescriptor<User>(
+            predicate: #Predicate {
+                $0.username == usernameInput
+            }
+        )
+        do {
+            let users = try context.fetch(fetchDescription)
+            if users.isEmpty {
+                user = User(username: usernameInput, password: passwordInput)
+                isLoggedIn = true
+            } else {
+                loginError = "Username already taken"
+            }
+        } catch {
+            loginError = "Error fetching users: \(error)"
+        }
     }
+//    func login(context: ModelContext) {
+//        if authModel.validateCredentials() {
+//            user = User(username: usernameInput, password: passwordInput)
+//            isLoggedIn = true
+//            loginError = ""
+//        } else {
+//            loginError = "Invalid username or password"
+//        }
+//    }
+//
+//    func register(context: ModelContext) {
+//        // Hier Registrierung abfertigen
+//        user = User(username: usernameInput, password: passwordInput)
+//        isLoggedIn = true
+//        loginError = ""
+//    }
 
     func logout() {
         isLoggedIn = false
